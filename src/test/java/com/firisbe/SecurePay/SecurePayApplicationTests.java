79f335afac67c67c5fb44e77807846b143bff56a
ardaltinay <arda.altinay@ozguryazilim.com.tr>
2024-03-26 19:03:43 +0300

    set up properties file

---
+++ src/test/java/com/firisbe/SecurePay/SecurePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firisbe.SecurePay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
